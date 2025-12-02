c74a5321c8eca067f72e8b5072d1f9e70d7c3c4d
AaryanBasnet <basnetaryan1011@gmail.com>
2025-12-02 22:19:49 +0545

    Add API configuration and integrate React Query with authentication and toast notifications

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+// import App from "./App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AuthProvider from "./auth/AuthProvider";
+import AppRouter from "./routers/AppRouter";
+import { ToastContainer } from "react-toastify";
 
-createRoot(document.getElementById('root')).render(
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <AppRouter />
+
+        <ToastContainer
+          position="top-center"
+          autoClose={2000}
+          hideProgressBar={false}
+          theme="dark"
+        />
+      </AuthProvider>
+    </QueryClientProvider>
+  </StrictMode>
+);
